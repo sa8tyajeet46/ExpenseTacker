61a69ed819dfad78d853e37b8ac377e38d710024
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2023-06-18 10:43:01 +0530

    kl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 const dotenv = require("dotenv");
 const connectDb = require("./connectDatabase");
-dotenv.config({ path: "./config/.env" });
+// dotenv.config({ path: "./config/.env" });
 
 app.listen(process.env.PORT, () => {
   console.log(`app is running at port ${process.env.PORT}`);
